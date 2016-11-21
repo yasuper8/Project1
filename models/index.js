1da758e01b6935a1125273189fa91a2008a2e248
yasuper8 <ysuper8@gmail.com>
2016-11-21 13:59:58 -0800

    updated index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/test");
 
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/signup" );
 
 var User = require('./user.js');
 module.exports.User = User;
