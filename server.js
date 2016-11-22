afffd04319c5c465ff2c65fe15b3d321d17489cf
yasuper8 <ysuper8@gmail.com>
2016-11-21 16:27:37 -0800

    fixed giphy accessing issue

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
