946e5f4599b2f53fd6a3219588661d78ce163f3f
yasuper8 <ysuper8@gmail.com>
2016-11-21 21:27:18 -0800

    CORS header test taking Content-Type out

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type"); //Access-Control-Request-Headers  Access-Control-Request-Method
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With"); //Access-Control-Request-Headers  Access-Control-Request-Method  Content-Type
   next();
 });
 
