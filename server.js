aa880f8f6551e0956758bb6bc0058e6ea4bb5d9e
yasuper8 <ysuper8@gmail.com>
2016-11-21 16:16:24 -0800

    more access-control headers

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set('view engine', 'ejs');
 // allow cross origin requests (optional)
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
 app.use(function(req, res, next) {
-  response.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
